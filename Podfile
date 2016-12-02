51bcf50bbc71e655b1b37cb4c8d7b7b38c21480f
Joan Raychouni <joan.raychouni@mi24.tv>
2016-12-02 09:05:04 +0100

    basic ui

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '10.1'
+
+target 'howwefeel' do
+  pod 'Charts', '~> 3.0.1'
+  pod 'Charts', '~> 3.0.1'
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for howwefeel
+
+  target 'howwefeelTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
+workspace 'howwefeel'
---
+++ howwefeel/PersonTodayVC.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  howwefeel
+//
+//  Created by joan on 01/12/2016.
+//  Copyright © 2016 joan. All rights reserved.
+//
+
+import UIKit
+
+class PersonTodayVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
